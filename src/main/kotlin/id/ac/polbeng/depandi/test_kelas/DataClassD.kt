77e714aa1238404a853009f9a532a99d1bee7961
Melia Syhada <meliasyuhada5@gmail.com>
2022-04-03 10:11:50 +0700

    32. DataClass[D] Data Class toString Method

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/DataClassD.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.depandi.test_kelas
+
+//DataClass toString() method
+// toString berfungsi untuk mengembalikan nilai kembalian string pada data class yang merupakan representasi dari objek.
+
+fun main() {
+    val lia = Student("Lia", 21)
+    println(lia.toString())
+}
